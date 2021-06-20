ef7a92bd82108f02ffbefc5db6ecbda747382130
Jacob Friis Saxberg <jacob@webcom.dk>
2021-06-20 16:37:43 +0200

    reformat

--- src/sbp2json/generators.mjs
+++ src/sbp2json/generators.mjs
@@ -7,8 +7,8 @@ const SIZE_UPPER = Math.max(SIZE_HEADER, SIZE_CHUNK); // bytes, octets
 
 const parseHeader = (chunkHeader) => {
 
-    // TODO: parse header
-    return Object.freeze({});
+  // TODO: parse header
+  return Object.freeze({});
 
 };
 
@@ -17,134 +17,134 @@ export const ERROR_INVALID_HEADER = 'invalid header inside file!';
 
 export const nextChunk = (fileDescriptor, outputBuffer, chunkLength) => {
 
-    const callback = (resolve, reject) => (error, bytesRead) => {
+  const callback = (resolve, reject) => (error, bytesRead) => {
 
-        if (error) {
-            reject(error);
-            return;
-        }
+    if (error) {
+      reject(error);
+      return;
+    }
 
-        resolve(bytesRead);
+    resolve(bytesRead);
 
-    };
+  };
 
-    const promiseBody = (resolve, reject) => {
+  const promiseBody = (resolve, reject) => {
 
-        fs.read(
-            fileDescriptor,
-            outputBuffer,
-            0,
-            chunkLength,
-            null,
-            callback(resolve, reject),
-        );
+    fs.read(
+      fileDescriptor,
+      outputBuffer,
+      0,
+      chunkLength,
+      null,
+      callback(resolve, reject),
+    );
 
-    };
+  };
 
-    return new Promise(promiseBody);
+  return new Promise(promiseBody);
 
 };
 
 export async function* chunksGenerator(fileDescriptor) {
 
-    const buffer = Buffer.alloc(SIZE_UPPER);
+  const buffer = Buffer.alloc(SIZE_UPPER);
 
-    // reads header from file descriptor
-    const bytesRead = await nextChunk(fileDescriptor, buffer, SIZE_HEADER);
+  // reads header from file descriptor
+  const bytesRead = await nextChunk(fileDescriptor, buffer, SIZE_HEADER);
 
-    if (bytesRead !== SIZE_HEADER) { // failed to fully read header
-        throw new Error(ERROR_INCORRECT_HEADER_SIZE);
+  if (bytesRead !== SIZE_HEADER) { // failed to fully read header
+    throw new Error(ERROR_INCORRECT_HEADER_SIZE);
+  }
+
+  // if failed to parse header
+  if (!parseHeader(buffer)) {
+    throw new Error(ERROR_INVALID_HEADER);
+  }
+
+  // yields the header chunk (32 bytes)
+  yield buffer;
+
+  let chunksLeft = true;
+
+  while (chunksLeft) {
+
+    const bytesRead = await nextChunk(fileDescriptor, buffer, SIZE_CHUNK);
+
+    if (bytesRead === 0) { // no more chunks to be read
+      break;
     }
 
-    // if failed to parse header
-    if (!parseHeader(buffer)) {
-        throw new Error(ERROR_INVALID_HEADER);
+    if (bytesRead !== SIZE_CHUNK) { // failed to fully read an entry
+      throw new Error();
     }
 
-    // yields the header chunk (32 bytes)
+    // yields the received chunk (64 bytes)
     yield buffer;
 
-    let chunksLeft = true;
-
-    while (chunksLeft) {
-
-        const bytesRead = await nextChunk(fileDescriptor, buffer, SIZE_CHUNK);
-
-        if (bytesRead === 0) { // no more chunks to be read
-            break;
-        }
-
-        if (bytesRead !== SIZE_CHUNK) { // failed to fully read an entry
-            throw new Error();
-        }
-
-        // yields the received chunk (64 bytes)
-        yield buffer;
-
-    }
+  }
 
 }
 
 export async function* tracksGenerator(fileDescriptor) {
 
-    const createFeature = (startChunk) => {
+  const createFeature = (startChunk) => {
 
-        const name = Parser.chunkName(startChunk);
-        const time = Parser.chunkTime(startChunk);
-        const coordinates = Parser.chunkCoordinates(startChunk);
-    
-        const feature = {
-            'type': 'Feature',
-            'properties': {
-                'name': name,
-                'time': time,
-                'coordTimes': [time],
-            },
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [coordinates],
-            },
-        };
-    
-        return feature;
-    
+    const name = Parser.chunkName(startChunk);
+    const time = Parser.chunkTime(startChunk);
+    const coordinates = Parser.chunkCoordinates(startChunk);
+
+    const feature = {
+      'type': 'Feature',
+      'properties': {
+        'name': name,
+        'time': time,
+        'coordTimes': [time],
+      },
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': [coordinates],
+      },
     };
 
-    const chunkStream = chunksGenerator(fileDescriptor);
+    return feature;
 
-    await chunkStream.next(); // ignores header
+  };
 
-    const { value: startChunk, done } = await chunkStream.next();
+  const chunkStream = chunksGenerator(fileDescriptor);
 
-    if (done) {
-        return;
-    }
+  await chunkStream.next(); // ignores header
 
-    let feature = createFeature(startChunk);
+  const { value: startChunk, done } = await chunkStream.next();
 
-    const timesArray = feature['properties']['coordTimes'];
-    const coordinatesArray = feature['geometry']['coordinates'];
+  if (done) {
+    return;
+  }
 
-    for await (const chunk of chunkStream) {
+  let feature = createFeature(startChunk);
 
-        const time = Parser.chunkTime(chunk);
-        const coordinates = Parser.chunkCoordinates(chunk);
-    
-        timesArray.push(time);
-        coordinatesArray.push(coordinates);
+  const timesArray = feature['properties']['coordTimes'];
+  const coordinatesArray = feature['geometry']['coordinates'];
 
-    }
+  for await (const chunk of chunkStream) {
 
-    yield feature;
+    const time = Parser.chunkTime(chunk);
+    const coordinates = Parser.chunkCoordinates(chunk);
+
+    timesArray.push(time);
+    coordinatesArray.push(coordinates);
+
+  }
+
+  yield feature;
 
 }
 
 const Generators = Object.freeze({
-    ERROR_INCORRECT_HEADER_SIZE,
-    ERROR_INVALID_HEADER,
-    nextChunk,
-    chunksGenerator,
-    tracksGenerator,
+  ERROR_INCORRECT_HEADER_SIZE,
+  ERROR_INVALID_HEADER,
+  nextChunk,
+  chunksGenerator,
+  tracksGenerator,
 });
 
 export default Generators;
