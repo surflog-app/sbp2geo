5f822219e145820fd7a906fc6931e99f2917e016
Jacob Friis Saxberg <jacob@webcom.dk>
2021-06-20 18:57:36 +0200

    stream example

--- src/cli.js
+++ src/cli.js
@@ -1,75 +1,23 @@
-import fs from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 import SBP2JSON from './sbp2json/index.mjs';
 
 const main = (argc, argv) => {
 
-    if (argc !== 4) {
-        console.log('Usage: node main.mjs <input-file> <output-file>');
-        return;
-    }
+  if (argc !== 4) {
+    console.log('Usage: node main.mjs <input-file> <output-file>');
+    return;
+  }
 
-    const inputFile = argv[2];
-    const outputFile = argv[3];
+  const inputFile = argv[2];
+  const outputFile = argv[3];
 
-    const open = (fileName, flags) => {
-
-        return new Promise((resolve, reject) => {
-
-            fs.open(fileName, flags, (error, fileDescriptor) => {
-
-                if (error) {
-
-                    reject(error);
-
-                } else {
-
-                    resolve(fileDescriptor);
-
-                }
-                
-            });
-
-        });
-
-    };
-
-    const write = (fileName, content) => {
-
-        return new Promise((resolve, reject) => {
-
-            fs.writeFile(fileName, content, (error) => {
-
-                if (error) {
-
-                    reject(error);
-
-                } else {
-
-                    resolve();
-
-                }
-
-            });
-
-        });
-
-    };
-
-    const promise = (async () => {
-
-        const fileDescriptor = await open(inputFile, 'r');
-
-        const object = await SBP2JSON.convertFile(fileDescriptor);
-
-        const content = JSON.stringify(object, null, 2);
-
-        await write(outputFile, content);
-
-    })();
-
-    promise.catch((error) => {
-        console.error('[ERROR]: ', error);
-    });
+  const streamRead = createReadStream(inputFile);
+  const streamWrite = createWriteStream(outputFile);
+  streamRead.pipe(streamWrite);
+  // SBP2JSON.convertStream(streamRead, streamWrite);
+  streamWrite.on('finish', () => {
+    console.log('All writes are now complete.');
+  });
 
 };
 
